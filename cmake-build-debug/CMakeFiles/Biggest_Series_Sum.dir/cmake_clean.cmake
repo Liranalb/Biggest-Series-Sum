e0959bac86cb173c91ef92236e9f4a684a665f3a
liran <liran.alb@gmail.com>
2018-12-22 11:22:26 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/Biggest_Series_Sum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Biggest_Series_Sum.dir/ex2a.c.o"
+  "Biggest_Series_Sum.pdb"
+  "Biggest_Series_Sum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Biggest_Series_Sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
